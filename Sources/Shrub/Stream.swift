dfec72624a5b03f45e3ccb5297c4caadde2ec54f
milos <milos@screensailor.net>
2021-03-05 08:32:49 +0000

    thinking about streams and publishers

--- Sources/Shrub/Stream.swift
+++ Sources/Shrub/Stream.swift
@@ -12,15 +12,15 @@ extension Publisher {
     }
 }
 
-extension Publisher where Output: Wave {
-    
+extension Publisher where Output: Wave, Failure == Never {
+
     public func get() -> AnyPublisher<Output.Value, Error> {
         self
             .tryMap{ try $0.get() }
             .eraseToAnyPublisher()
     }
     
-    public func map<A>(_ ƒ: @escaping (Output.Value) throws -> A) -> AnyPublisher<Result<A, Error>, Failure> {
+    public func map<A>(_ ƒ: @escaping (Output.Value) throws -> A) -> Stream<A> {
         self
             .map{ x in Result{ try ƒ(x.get()) } }
             .eraseToAnyPublisher()
