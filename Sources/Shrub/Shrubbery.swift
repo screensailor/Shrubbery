dc63f7af7135c32c7f7ab509392d31d7ab06737d
milos <milos@screensailor.net>
2021-03-06 16:46:56 +0000

    thinking about Code and Encoded as specialisations of Shrub and Shrubbery

--- Sources/Shrub/Shrubbery.swift
+++ Sources/Shrub/Shrubbery.swift
@@ -1,17 +1,20 @@
 /**
  * - events vs? streams - i.e. events as streams of () or of Event value?
  * - subscribing vs observing
- * - decoder & encoder of Shrubbery
+ * - concurrent and serial dependencies - i.e. group vs sequence
  * - flat collections (path components) vs deep documents (values)
+ * - decoder & encoder of ``Shrubbery`` (`as _: A.Type` tries a cast then decode where `A: Decodable`)
  */
-public typealias Code = Shrub
-public typealias Encoded = Shrubbery
+public typealias Code = Shrub<String, Codable>
+public protocol Encoded: Shrubbery where Key == String, Value: Codable {}
 
 extension Shrub: Shrubbery {}
 
 public protocol Shrubbery {
     
     associatedtype Key: Hashable
+    associatedtype Value
+    
     typealias Index = EitherType<Int, Key>
     
     func get<A, Path>(_ path: Path, as: A.Type) throws -> A
