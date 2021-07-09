5f35edc2c52880dfdddb636a6f88143783a7b56d
Milos <milos@screensailor.net>
2021-07-09 20:44:50 +0100

    thinking about thread safety

--- Sources/Shrub/Pond.swift
+++ Sources/Shrub/Pond.swift
@@ -19,16 +19,22 @@ where
 
     public let geyser: Source
     
-    private var basin: Basin = .init()
-    private var subscriptions: Tree<Fork, Subscription> = .init()
+    public private(set) var basin: Basin
+    public private(set) var subscriptions: Tree<Fork, Subscription>
     
-    private let q: DispatchQueue = .init(
+    private let q: DispatchQueue = .init( // TODO:❗️ generalise as Scheduler
         label: "\(Pond<Source, Key>.self).q_\(#file)_\(#line)",
         qos: .userInteractive
     )
     
-    public init(geyser: Source) {
+    public init(
+        geyser: Source,
+        basin: Basin = .init(),
+        subscriptions: Tree<Fork, Subscription> = .init()
+    ) {
         self.geyser = geyser
+        self.basin = basin
+        self.subscriptions = subscriptions
     }
     
     public func flow<A>(of route: Route, as: A.Type) -> Flow<A> {
@@ -38,40 +44,42 @@ where
         do {
             let endIndex = try geyser.source(of: route)
             guard endIndex >= route.startIndex else {
-                return "Invalid end index of the source of route \(route)".error().flow()
+                return "Invalid source end index of route \(route)".error().flow()
             }
             source = route[..<endIndex].array
-        }
-        catch {
+        } catch {
             return error.flow()
         }
         
         return flow(of: route, from: source, as: A.self)
     }
-        
+
     private func flow<A>(of route: Route, from source: Route, as: A.Type) -> Flow<A> {
         q.sync {
-            guard let subscription = subscriptions[value: source] else {
-                var count = 0
-                let cancel = { (x: Int) in
+            if let subscription = subscriptions[value: source] {
+                return subscription.didSink.flow(of: route, in: basin, cancel: subscription.cancel)
+            }
+            var count = 0
+            let cancel = { [weak self] (x: Int) in
+                guard let self = self else { return }
+                self.q.sync {
                     count += x
                     if count < 1 {
                         assert(count == 0)
                         self.subscriptions[value: source] = nil
                     }
                 }
-                let didSink = Subject(false)
-                subscriptions[value: source] = Subscription(
-                    cancel: cancel,
-                    didSink: didSink,
-                    cancellable: geyser.gush(of: source).sink{ [weak didSink] result in
-                        self.basin.set(source, to: result)
-                        didSink?.send(true)
-                    }
-                )
-                return didSink.flow(of: route, in: basin, cancel: cancel)
             }
-            return subscription.didSink.flow(of: route, in: basin, cancel: subscription.cancel)
+            let didSink = Subject(false)
+            subscriptions[value: source] = Subscription(
+                cancel: cancel,
+                didSink: didSink,
+                cancellable: geyser.gush(of: source).sink{ [weak didSink] result in
+                    didSink?.send(true)
+                    self.basin.set(source, to: result)
+                }
+            )
+            return didSink.flow(of: route, in: basin, cancel: cancel)
         }
     }
 }
@@ -83,13 +91,14 @@ private extension CurrentValueSubject where Output == Bool, Failure == Never {
         in basin: DeltaShrub<Key>,
         cancel: @escaping (Int) -> ()
     ) -> Flow<A> {
-        self.first(where: { $0 }).flatMap{ _ in
-            basin.flow(of: route)
-        }
-        .handleEvents(
-            receiveSubscription: { _ in cancel(+1) },
-            receiveCancel: { cancel(-1) }
-        )
-        .eraseToAnyPublisher()
+        first(where: { $0 })
+            .flatMap{ _ in
+                basin.flow(of: route)
+            }
+            .handleEvents(
+                receiveSubscription: { _ in cancel(+1) },
+                receiveCancel: { cancel(-1) }
+            )
+            .eraseToAnyPublisher()
     }
 }
